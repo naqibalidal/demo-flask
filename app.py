d41e78e1538ea5586d237153c6830d72401cde6a
naqibalidal <nq570360@dal.ca>
2025-09-24 12:49:37 -0300

    Some changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask from Google kubernetes cluster!!"
+    return "Flask from Google kubernetes clusters!!"
 
 
 if __name__ == "__main__":
